c93125f0098344f90c015da302f56438adf3129f
Daneel S. Yaitskov <dy@hivext.net>
2012-12-24 18:01:27 +0400

    XA JTA TRANSActions! JMS and MYSQL~gistgistgist

--- spring-activemq-sample/src/main/java/com/blog/spring/jms/MsgCtl.java
+++ spring-activemq-sample/src/main/java/com/blog/spring/jms/MsgCtl.java
@@ -3,8 +3,11 @@ package com.blog.spring.jms;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,23 +27,34 @@ public class MsgCtl {
     private JmsTemplate jmsTemplate;
 
     @Resource
+    private JdbcTemplate jdbcTemplate;
+
+    @Resource
     private Queue testQueue;
 
     private static final Logger logger = LoggerFactory.getLogger(MsgCtl.class);
 
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @RequestMapping("/send")
     public void send(HttpServletResponse response, @RequestParam("text") String text)
             throws JMSException, IOException {
-        jmsTemplate.convertAndSend(testQueue, text);
 
-        response.getWriter().append("ok. message '" + text + "' was sent");
+        jdbcTemplate.update("insert into msg (txt) values(?)", new Object[] { text });
+        int id = jdbcTemplate.queryForInt( "select last_insert_id()" );
+        jmsTemplate.convertAndSend(testQueue, id);
+        response.getWriter().append("ok. message '" + text + "' was sent and id = " + id);
 
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @RequestMapping("/receive")
     public void receive(HttpServletResponse response) throws IOException {
         Object msg = jmsTemplate.receiveAndConvert(testQueue);
-        response.getWriter().append("ok. message '" + msg + "' is received");
+        logger.debug("got id " + msg);
+        int id = (Integer)msg;
+        String tmsg = (String) jdbcTemplate.queryForObject("select txt from msg where id = ?", String.class, id);
+        jdbcTemplate.update("delete from msg where id = ?", id);
+        response.getWriter().append("ok. message '" + tmsg + "' is received with id = " + id);
     }
 }
